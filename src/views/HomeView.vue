47cffedf901ef9dfb53b9c6ef6bd89a0423bd711
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-09-25 15:53:52 -0300

    feat: quasar added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-tabs align="left">
+        <q-route-tab to="/" label="Page One" />
+        <q-route-tab to="/about" label="Page Two" />
+      </q-tabs>
+    </q-header>
+  </q-layout>
 </template>
